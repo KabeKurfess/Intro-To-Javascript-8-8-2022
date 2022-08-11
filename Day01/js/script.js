37dc96f887eb5cad7f4768f6ed34a956d2707702
KabeKurfess <kabekurfess@gmail.com>
2022-08-10 20:28:29 -0700

    Day02 Commit

--- Day01/js/script.js
+++ Day01/js/script.js
@@ -1,6 +1,12 @@
 // this is a single line comment
-/* this is a multi line comment */
-alert("Hello");
+/* this is a 
+multi line comment */
 
 console.log("Kabe Kurfess");
 console.log("Mexican");
+
+var name = "Kabe Kurfess";
+var food = "Mexican";
+
+console.log(name);
+console.log(food);
---
+++ Day02/js/script.js
@@ -0,0 +1,30 @@
+// var 
+// let 
+// const 
+// var is a keyword to declare a variable in javascript  
+// let is a keyword to declare a variable in javascript and it is block scoped
+// const is a keyword to declare a variable in javascript and it is immutable
+
+// data types in javascript
+// string - "Hello World" (text) (double quotes)
+// number - 123, (integer) (infinity positive and negative) (NaN)  
+// boolean - true or false
+// undefined - undefined (not assigned)
+// null - null (no value)
+// bigint - bigint (very large integers)
+// symbol - symbol (random number)  
+// object - object (key value pair)
+// array - array (list of values) 
+// operator - + - * / % (modulus)
+
+// expression - expression is a combination of variables and operators
+
+let longtext = "Supercalifragilisticexpialidocious";
+let wordlength = longtext.length;
+console.log(wordlength);
+const number = 123;
+console.log(number);
+const bigint = 9283475983745980723980459082347590827359082374590872345;
+console.log(bigint);
+
+
